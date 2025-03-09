282aaf590aa3ecb97b361797dbaa649c34cc5fb6
gitcode <website0wordpress@gmail.com>
2025-03-09 15:33:33 +0530

    feat: added s3 download

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { sql } from 'bun';
+import { sql, s3 } from 'bun';
 import { Hono } from 'hono';
 
 const app = new Hono();
@@ -12,4 +12,21 @@ app.get('/', async (c) => {
   }
 });
 
+app.get('/image', async (c) => {
+  try {
+    const imageFile = s3.file("pn.jpeg");    
+    const imageData = await imageFile.arrayBuffer();
+    await Bun.write("local-image.jpg", new Uint8Array(imageData));
+    return c.json({ success: "wow worked" }, 200);
+  } catch (error) {
+    return c.json({ error: (error as Error).message }, 500);
+  }
+});
+
 export default app;
+
+
+// get object download presigned url
+// get object presigned url with expiry 
+
+// upload object with presigned url with expiry
